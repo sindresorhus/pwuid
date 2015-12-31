c305fb174ca4bbefe999d2e853ea5768037ad64c
Sindre Sorhus <sindresorhus@gmail.com>
2015-12-31 21:26:37 +0100

    update tests for latest AVA version

--- test.js
+++ test.js
@@ -3,11 +3,8 @@ import fn from './';
 
 test(t => {
 	const pwuid = fn();
-
 	console.log(pwuid);
-
 	t.is(typeof pwuid, 'object');
 	t.is(typeof pwuid.name, 'string');
 	t.is(typeof pwuid.dir, 'string');
-	t.end();
 });
